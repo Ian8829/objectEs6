a05211f394a647bdf9576cb96d4d682220169d25
taeshik Hwang <ghjkghj@naver.com>
2017-11-21 17:21:51 +0900

    maps and weakmaps 2

--- mapsWeakMaps.js
+++ mapsWeakMaps.js
@@ -1,10 +1,18 @@
 const myMap = new Map();
 
 //API
+// set(), get(), size, clear(), has()
 
 myMap.set('foo', 'bar');
 myMap.set('hello', 'world');
 
 // myMap.clear();
 
-console.log(myMap.has('hello'));
+// console.log(myMap.has('hello'));
+
+//Iterators
+// keys(), entries(), values
+
+for(let value of myMap.values()) {
+  console.log(value);
+}
